3e8ff49515648627379380c51de5ce74598cd8a4
Matthew Stiles <mstiles92@gmail.com>
2024-02-04 20:44:38 -0500

    feat: read local dirs, pass to fzf and receive selection

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/adrg/xdg"
 	"github.com/spf13/cobra"
@@ -15,8 +20,61 @@ var (
         Use: "sessionizer",
         Short: "A smart terminal session manager",
         Run: func(cmd *cobra.Command, args []string) {
-            fmt.Println("Main command starts here")
-            fmt.Println("Config file:", cfgFile)
+            var fuzzyFindEntries []string
+
+            localDirs := viper.GetStringSlice("localDirs")
+
+            for _, localDir := range localDirs {
+                files, err := os.ReadDir(localDir)
+                if err != nil {
+                    log.Fatalln(err)
+                }
+
+                for _, file := range files {
+                    if file.IsDir() {
+                        fuzzyFindEntries = append(fuzzyFindEntries, localDir + "/" + file.Name())
+                    }
+                }
+            }
+
+            if _, err := exec.LookPath("fzf"); err != nil {
+                log.Fatalln("fzf is not installed or could not be found in $PATH")
+            }
+
+            fzf := exec.Command("fzf")
+            stdin, err := fzf.StdinPipe()
+            if err != nil {
+                 log.Fatalln(err)
+            }
+            defer stdin.Close()
+
+            fzf.Stderr = os.Stderr
+
+            stdout, err := fzf.StdoutPipe()
+            if err != nil {
+                 log.Fatalln(err)
+            }
+            defer stdout.Close()
+
+            if err = fzf.Start(); err != nil {
+                log.Fatalln(err)
+            }
+
+            _, err = io.WriteString(stdin, strings.Join(fuzzyFindEntries, "\n"))
+            if err != nil {
+                 log.Fatalln(err)
+            }
+
+            selected, err := io.ReadAll(stdout)
+            if err != nil {
+                 log.Fatalln(err)
+            }
+
+            if err = fzf.Wait(); err != nil {
+                log.Fatalln(err)
+            } 
+
+            fmt.Println("Selected option:", strings.TrimSpace(string(selected)))
         },
     }
 )
@@ -45,5 +103,8 @@ func initConfig() {
         fmt.Println("Using config file:", viper.ConfigFileUsed())
     } else {
         fmt.Println("Error:", err)
+
+        // not able to load config, set defaults for anything required here:
+        viper.Set("localDirs", []string{xdg.Home})
     }
 }

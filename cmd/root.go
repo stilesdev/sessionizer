a97bb2666dcc3d5027929b157bbd94f864b0dd82
Matthew Stiles <mstiles92@gmail.com>
2024-02-06 21:52:00 -0500

    refactor: move fzf command into separate package

--- cmd/root.go
+++ cmd/root.go
@@ -2,10 +2,8 @@ package cmd
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 
@@ -13,6 +11,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/stilesdev/sessionizer/multiplexers/tmux"
+	"github.com/stilesdev/sessionizer/prompts/fzf"
 )
 
 var (
@@ -43,62 +42,29 @@ var (
                 }
             }
 
-            if _, err := exec.LookPath("fzf"); err != nil {
+
+
+
+            if !fzf.IsAvailable() {
                 log.Fatalln("fzf is not installed or could not be found in $PATH")
             }
 
-            // TODO: figure out how to dynamically change prompt or something to indicate that a generic session will be created with the entered query as the name
-            fzf := exec.Command("fzf", "--exact", "--print-query")
-            stdin, err := fzf.StdinPipe()
+            selectedOption, enteredQuery, err := fzf.Prompt(fuzzyFindEntries)
             if err != nil {
-                 log.Fatalln(err)
-            }
-            defer stdin.Close()
-
-            fzf.Stderr = os.Stderr
-
-            stdout, err := fzf.StdoutPipe()
-            if err != nil {
-                 log.Fatalln(err)
-            }
-            defer stdout.Close()
-
-            if err = fzf.Start(); err != nil {
                 log.Fatalln(err)
             }
-
-            _, err = io.WriteString(stdin, strings.Join(fuzzyFindEntries, "\n"))
-            if err != nil {
-                 log.Fatalln(err)
-            }
-
-            fzfResult, err := io.ReadAll(stdout)
-            if err != nil {
-                 log.Fatalln(err)
-            }
-
-            if err = fzf.Wait(); err != nil {
-                // fzf still returns a non-zero exit code when nothing selected, but need to ignore that so this can still read the entered query and create a generic session
-                //log.Fatalln(err)
-            } 
-
-            selected := strings.TrimSpace(string(fzfResult))
 
             var sessionName string
             var sessionPath string
 
-            lines := strings.Split(string(fzfResult), "\n")
-            fmt.Println("Split result:", lines, len(lines))
-            if len(lines) == 2 {
-                fmt.Println("Create new generic session with name:", lines[0])
-                sessionName = lines[0]
-                sessionPath = xdg.Home
-            } else if len(lines) == 3 {
-                fmt.Println("Selected entry from list:", selected)
-                sessionName = filepath.Base(selected)
-                sessionPath = lines[1]
+            if selectedOption != "" {
+                fmt.Println("Selected entry from list:", selectedOption)
+                sessionName = filepath.Base(selectedOption)
+                sessionPath = selectedOption
             } else {
-                log.Fatalln("Invalid result returned from fzf")
+                fmt.Println("Create new generic session with name:", enteredQuery)
+                sessionName = enteredQuery
+                sessionPath = xdg.Home
             }
 
 
---
+++ prompts/fzf/fzf.go
@@ -0,0 +1,66 @@
+package fzf
+
+import (
+	"errors"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func IsAvailable() bool {
+    if _, err := exec.LookPath("fzf"); err != nil {
+        return false
+    }
+
+    return true
+}
+
+func Prompt(options []string) (string, string, error) {
+    // TODO: figure out how to dynamically change prompt or something to indicate that a generic session will be created with the entered query as the name
+    fzf := exec.Command("fzf", "--exact", "--print-query")
+    stdin, err := fzf.StdinPipe()
+    if err != nil {
+        return "", "", err
+    }
+    defer stdin.Close()
+
+    fzf.Stderr = os.Stderr
+
+    stdout, err := fzf.StdoutPipe()
+    if err != nil {
+        return "", "", err
+    }
+    defer stdout.Close()
+
+    if err = fzf.Start(); err != nil {
+        return "", "", err
+    }
+
+    _, err = io.WriteString(stdin, strings.Join(options, "\n"))
+    if err != nil {
+        return "", "", err
+    }
+
+    fzfResult, err := io.ReadAll(stdout)
+    if err != nil {
+        return "", "", err
+    }
+
+    if err = fzf.Wait(); err != nil {
+        // fzf still returns a non-zero exit code when nothing selected, but need to ignore that so this can still read the entered query and create a generic session
+        //return "", "", err
+    } 
+
+    lines := strings.Split(string(fzfResult), "\n")
+
+    //fmt.Println("Split result:", lines, len(lines))
+    
+    if len(lines) == 2 {
+        return "", lines[0], nil
+    } else if len(lines) == 3 {
+        return lines[1], lines[0], nil
+    } else {
+        return "", "", errors.New("Invalid result returned from system fzf executable")
+    }
+}
